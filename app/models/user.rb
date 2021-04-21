ef8b2fa35d580008d685337634b55e350598474e
programmingnuoh <myth0725@gmail.com>
2021-04-21 21:15:32 +0900

    バリデーション設定、ビューファイル修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :name, presence: true
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+  validates :password, with: PASSWORD_REGEX, length: {minimum: 8}
 end
