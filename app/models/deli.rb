8011ea6a20c100b349ff25bc69431e35e7ec6164
programmingnuoh <myth0725@gmail.com>
2021-04-29 17:53:08 +0900

    active_storage,active_hash修正

--- app/models/deli.rb
+++ app/models/deli.rb
@@ -8,5 +8,7 @@ class Deli < ApplicationRecord
 
   belongs_to :user
   belongs_to :category
-  has_one_attaced :image
+  has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :supermarket
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,5 +9,4 @@ class User < ApplicationRecord
   validates_format_of :password, with: PASSWORD_REGEX, length: {minimum: 8}
 
   has_many :delis
-  has_one_attaced :image
 end
