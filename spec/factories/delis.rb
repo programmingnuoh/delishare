561532a78ce9b31d013cf959f932b8b89d2acd3f
programmingnuoh <myth0725@gmail.com>
2021-05-09 22:23:02 +0900

    FactoryBot設定

--- spec/factories/delis.rb
+++ spec/factories/delis.rb
@@ -1,10 +1,10 @@
 FactoryBot.define do
   factory :deli do
-    name {'test'}
-    text {'test'}
-    supermarket_id{rand(1..10)}
-    association :user
+    name { 'test' }
+    text { 'test' }
+    supermarket_id { 4 }
     association :category
+    association :user
     after(:build) do |deli|
       deli.image.attach(io: File.open("#{Rails.root}/public/testimage.jpg"), filename:'testimage.jpg')
     end
