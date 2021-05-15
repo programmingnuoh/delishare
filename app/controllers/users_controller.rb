74fde8f916a950c55d4e120b384d51b68d5164b1
programmingnuoh <myth0725@gmail.com>
2021-05-15 15:10:08 +0900

    コントローラー記述

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def show
-    user = User.find(params[:id])
-    @delis = user.delis
+    @user = User.find(params[:id])
+    @delis = @user.delis
   end
 end
