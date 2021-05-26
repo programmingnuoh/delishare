03f8fd8fb52070bfa4a3cffc1ca37c25ccf1d50c
programmingnuoh <myth0725@gmail.com>
2021-05-26 15:44:45 +0900

    モデル・コントローラー書き足し

--- app/controllers/delis_controller.rb
+++ app/controllers/delis_controller.rb
@@ -2,7 +2,6 @@ class DelisController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show, :search]
   before_action :deli_find, only: [:show, :edit, :update, :destroy, :confirm]
   before_action :user_check, only: [:edit, :destroy, :confirm]
-  # before_action :search_deli
 
   def index
     @delis = Deli.includes(:user).order("created_at DESC")
@@ -14,7 +13,8 @@ class DelisController < ApplicationController
 
   def create
     @deli = DelisTag.new(deli_params)
-    if @deli.save
+    if @deli.valid?
+      @deli.save
       redirect_to root_path
     else
       render :new
@@ -57,4 +57,5 @@ class DelisController < ApplicationController
       redirect_to deli_path(@deli.id) 
     end
   end
+
 end
--- app/models/delis_tag.rb
+++ app/models/delis_tag.rb
@@ -11,7 +11,26 @@ class DelisTag
 
   def save
     deli = Deli.create(name: name, text: text, category_id: category_id, supermarket_id: supermarket_id, image:image, user_id:user_id)
-    tag = Tag.create(tagname: tagname)
-    deli_tag = DeliTagRelation.create(deli_id: deli.id, tag_id: tag.id)
+    tag = Tag.where(tagname:tagname).first_or_initialize
+    tag.save
+    DeliTagRelation.create(deli_id: deli.id, tag_id: tag.id)
   end
+
+  # def update(tag_list)
+  #   ActiveRecord::Base.transaction do
+  #     @deli.update(name: name, text: text, category_id: category_id, supermarket_id: supermarket_id, image:image, user_id:user_id)
+
+  #     @deli.deli_tags.each do |tag|
+  #       tag.delete
+  #     end
+  #     tag_list.each do |tagname|
+  #       tag = Tag.where(tagname:tagname).first_or_initialize
+  #       tag.save
+
+  #       deli_tag = DeliTagRelation.where(deli_tag: @deli.tag, tag_id: tag.id).first_or_initialize
+  #       deli_tag.update(deli_id: @deli.id, tag_id: tag.id)
+  #     end
+  #   end
+  # end
+
 end
