aa2432c964f0d328f9266e425784e9be3135e82b
programmingnuoh <myth0725@gmail.com>
2021-06-25 22:54:51 +0900

    アニメーション追加

--- app/javascript/packs/index.js
+++ app/javascript/packs/index.js
@@ -1,3 +1,42 @@
+function messageLine(){
+    $('.site-message').each(function(){
+        const elemPos = $(this).offset().top-50;
+        const scroll = $(window).scrollTop();
+        const windowHeight = $(window).height();
+        if (scroll >= elemPos - windowHeight) {
+          $(this).addClass('message-line-appear')
+        } else {
+          $(this).removeClass('message-line-appear')
+        }
+});
+}
+
+function titleLine(){
+    $('.introduction-title').each(function(){
+        const elemPos = $(this).offset().top-50;
+        const scroll = $(window).scrollTop();
+        const windowHeight = $(window).height();
+        if (scroll >= elemPos - windowHeight) {
+          $(this).addClass('title-line-move')
+        } else {
+          $(this).removeClass('title-line-move')
+        }
+    });
+}
+
+function deliFade(){
+    $('.deli-list').each(function(){
+        const elemPos = $(this).offset().top-50;
+        const scroll = $(window).scrollTop();
+        const windowHeight = $(window).height();
+        if (scroll >= elemPos - windowHeight) {
+          $(this).addClass('deli-fadeUp')
+        } else {
+          $(this).removeClass('deli-fadeUp')
+        }
+    });
+}
+
 $(window).on('load', function(){
   $('.main-message').each(function(){
     const elemPos = $(this).offset().top-50;
@@ -12,24 +51,7 @@ $(window).on('load', function(){
 });
 
 $(window).scroll(function(){
-    $('.site-message').each(function(){
-        const elemPos = $(this).offset().top-50;
-        const scroll = $(window).scrollTop();
-        const windowHeight = $(window).height();
-        if (scroll >= elemPos - windowHeight) {
-          $(this).addClass('message-line-appear')
-        } else {
-          $(this).removeClass('message-line-appear')
-        }
-      });
-    $('.introduction-title').each(function(){
-    const elemPos = $(this).offset().top-50;
-    const scroll = $(window).scrollTop();
-    const windowHeight = $(window).height();
-    if (scroll >= elemPos - windowHeight) {
-      $(this).addClass('title-line-move')
-    } else {
-      $(this).removeClass('title-line-move')
-    }
-  });
+  titleLine();
+  messageLine();
+  deliFade();
 });
